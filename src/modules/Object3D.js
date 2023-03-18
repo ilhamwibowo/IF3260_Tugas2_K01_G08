b0ab481178a93b12accb37e3987a7946a70b818d
ilhamwibowo <ilhamprasetyowb@gmail.com>
2023-03-19 00:17:33 +0700

    feat : set up

---
+++ src/modules/Object3D.js
@@ -0,0 +1,89 @@
+// Object3D.js
+import { m4 } from "./utils/mat4.js";
+/** Problem : 
+1. Transformations only done in modelMatrix.  
+2. Algorihtm to get normals from vertices and indices not yet implemented.
+**/
+
+class Object3D {
+  constructor(gl, vertices, colors, indices, normals, shaderProgram) {
+    this.gl = gl;
+    this.indices = indices;
+    this.shaderProgram = shaderProgram;
+    
+    // Model matrix representing object's transformations
+    this.modelMatrix = m4.identity(); 
+    this.initBuffers(vertices, colors, indices, normals);
+  }
+
+  // Transformations
+  translate(dx, dy, dz) {
+    this.modelMatrix = m4.translate(this.modelMatrix, dx, dy, dz);
+  }
+
+  rotateX(angle) {
+    this.modelMatrix = m4.xRotate(this.modelMatrix, angle);
+  }
+
+  rotateY(angle) {
+    this.modelMatrix = m4.yRotate(this.modelMatrix, angle);
+  }
+
+  rotateZ(angle) {
+    this.modelMatrix = m4.zRotate(this.modelMatrix, angle);
+  }
+
+  scale(sx, sy, sz) {
+    this.modelMatrix = m4.scale(this.modelMatrix, sx, sy, sz);
+  }
+
+  
+  // Initialize necessary buffers for object.
+  initBuffers(vertices, colors, indices, normals) {
+    this.buffers = {
+      position: this.createAndSetupBuffer(new Float32Array(vertices), this.gl.ARRAY_BUFFER),
+      color: this.createAndSetupBuffer(new Float32Array(colors), this.gl.ARRAY_BUFFER),
+      index: this.createAndSetupBuffer(new Uint16Array(indices), this.gl.ELEMENT_ARRAY_BUFFER),
+      normal: this.createAndSetupBuffer(new Float32Array(normals), this.gl.ARRAY_BUFFER),
+    };
+  }
+
+  // Bind and fill the buffer data.
+  createAndSetupBuffer(data, type) {
+    const buffer = this.gl.createBuffer();
+    this.gl.bindBuffer(type, buffer);
+    this.gl.bufferData(type, data, this.gl.STATIC_DRAW);
+    return buffer;
+  }
+
+  draw(matrix, normalMatrix, lightDirection, enableShading) {
+    // Tell webgl to use our program .
+    this.gl.useProgram(this.shaderProgram.program);
+
+    // Turn on, bind, and tell webgl to use the attributes.
+    this.gl.enableVertexAttribArray(this.shaderProgram.attributeLocations.position);
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.buffers.position);
+    this.gl.vertexAttribPointer(this.shaderProgram.attributeLocations.position, 3, this.gl.FLOAT, false, 0, 0);
+
+    this.gl.enableVertexAttribArray(this.shaderProgram.attributeLocations.color);
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.buffers.color);
+    this.gl.vertexAttribPointer(this.shaderProgram.attributeLocations.color, 4, this.gl.FLOAT, false, 0, 0);
+
+    this.gl.enableVertexAttribArray(this.shaderProgram.attributeLocations.normal);
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.buffers.normal);
+    this.gl.vertexAttribPointer(this.shaderProgram.attributeLocations.normal, 3, this.gl.FLOAT, false, 0, 0);
+
+    this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.buffers.index);
+
+    // Set uniforms.
+    this.gl.uniformMatrix4fv(this.shaderProgram.uniformLocations.normalMatrix, false, normalMatrix);
+    this.gl.uniformMatrix4fv(this.shaderProgram.uniformLocations.matrix, false, matrix);
+    this.gl.uniform3fv(this.shaderProgram.uniformLocations.lightDirection, lightDirection);
+    this.gl.uniform1i(this.shaderProgram.uniformLocations.enableShading, enableShading);
+
+    // Drawwzzz.
+    this.gl.drawElements(this.gl.TRIANGLES, this.indices.length, this.gl.UNSIGNED_SHORT, 0);
+    }       
+}
+
+export { Object3D };
---
+++ src/modules/main.js
@@ -0,0 +1,199 @@
+// main.js
+import { createCube } from "./models/basicCube.js";
+import { m4 } from "./utils/mat4.js";
+import { Object3D } from "./Object3D.js";
+import { Shader } from "./shaders/shaderProgram.js";
+import { loadFile } from "./utils/loader.js";
+
+/**
+ * run with http-server
+ * Need to process inputs.
+ * Event listeners not yet all implemented. 
+ *  
+ */
+
+const vertexShaderSource = `
+  attribute vec4 a_position;
+  attribute vec4 a_color;
+  attribute vec3 a_normal;
+
+  varying vec4 v_color;
+  varying vec3 v_normal;
+
+  uniform mat4 u_matrix;
+  uniform mat4 u_normalMatrix;
+
+  void main() {
+    gl_Position = u_matrix * a_position;
+    v_color = a_color;
+    v_normal = mat3(u_normalMatrix) * a_normal;
+  }
+`
+const fragmentShaderSource = `
+  precision mediump float;
+  
+  varying vec4 v_color;
+  varying vec3 v_normal;
+
+  uniform vec3 u_lightDirection;
+  uniform bool u_enableShading;
+
+  void main() {
+    vec3 normal = normalize(v_normal);
+    float lightIntensity = dot(normal, u_lightDirection);
+    vec4 shadedColor = vec4(v_color.rgb * lightIntensity, v_color.a);
+    gl_FragColor = u_enableShading ? shadedColor : v_color;
+  }
+`;
+
+function main() {
+    const canvas = document.querySelector('#canvas');
+    const gl = canvas.getContext('webgl');
+    const fileSelector = document.getElementById('fileInput');
+
+    if (!gl) {
+      console.error('WebGL not supported');
+      return;
+    }
+
+    // Set up shaders and cube properties
+    const shader = new Shader(gl, vertexShaderSource, fragmentShaderSource);
+    var { vertices, colors, indices, normals } = createCube();
+    var cube = new Object3D(gl, vertices, colors, indices, normals, shader);
+
+    let enableShading = false;
+    
+    // Set up camera properties
+    var cameraPosition = [0, 0, 5];
+    const target = [0, 0, 0];
+    const up = [0, 1, 0];
+
+    // Light direction
+    const lightDirection = [0.5, 0.7, 1];
+
+    // This is for testing purposes. Will be changed in future development
+    const cameraSpeed = 0.1;
+    document.addEventListener('keydown', (event) => {
+      if (event.key === 'r') {
+        rotate = !rotate;
+      }
+
+      else if (event.key === 'ArrowUp') {
+        cameraPosition[2] -= cameraSpeed;
+      } else if (event.key === 'ArrowDown') {
+        cameraPosition[2] += cameraSpeed;
+      } else if (event.key === 'ArrowLeft') {
+        cameraPosition[0] -= cameraSpeed;
+      } else if (event.key === 'ArrowRight') {
+        cameraPosition[0] += cameraSpeed;
+      } else if (event.key === "q" || event.key === "Q") {
+        enableShading = !enableShading;
+      } else if (event.key === "a" || event.key === "A") {
+        cube.translate(-0.5, 0, 0);
+      } else if (event.key === "d" || event.key === "D") {
+        cube.translate(0.5, 0, 0);
+      } else if (event.key === "s" || event.key === "S") {
+        cube.translate(0, 0, -0.5);
+      } else if (event.key === "w" || event.key === "W") {
+        cube.translate(0, 0, 0.5);
+      } else if (event.key === "t" || event.key === "t") {
+        cube.rotateX(0.1);
+        cube.rotateY(0.1);
+      } 
+
+      drawScene()
+    });
+    
+
+    /////////// Event Listeners ////////////
+    document.getElementById("loadButton").addEventListener("click", () => {
+      const fileInput = document.getElementById("fileInput");
+      const file = fileInput.files[0];
+  
+      if (!file) {
+          alert("No file selected!");
+          return;
+      }
+
+      loadFile(
+          file,
+          (fileContent) => {
+              cube = new Object3D(gl, fileContent.vertices, fileContent.colors, fileContent.indices, fileContent.normals, shader);
+              drawScene();
+          },
+          (error) => {
+              console.error("Error reading file:", error);
+          }
+      );
+    });
+
+    let rx_prev = 0;
+    document.getElementById("rx_slider").oninput = function () {
+      let value = document.getElementById("rx_slider").value;
+      cube.rotateX(value - rx_prev);
+      rx_prev = value;
+      drawScene();
+    };
+
+    let tx_prev = 0;
+    document.getElementById("tx_slider").oninput = function () {
+      let value = document.getElementById("tx_slider").value;
+      cube.translate(value - tx_prev, 0, 0);
+      tx_prev = value;
+      drawScene();
+    };
+
+    document.getElementById("shading").addEventListener("change", function() {
+      enableShading = this.checked ? true : false;
+      drawScene();
+    });
+    
+    function resizeCanvasToDisplaySize(canvas) {
+      const width = canvas.clientWidth * window.devicePixelRatio;
+      const height = canvas.clientHeight * window.devicePixelRatio;
+      if (canvas.width !== width || canvas.height !== height) {
+        canvas.width = width;
+        canvas.height = height;
+      }
+    }
+
+    
+    /////////// drawwwwzzzz /////////////////
+    // Draw scene for each frame
+    function drawScene() {
+      // Clear canvas and setup viewport.
+      resizeCanvasToDisplaySize(gl.canvas);
+      gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+      gl.clearColor(0.8, 0.8, 0.8, 1);
+      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+      gl.enable(gl.DEPTH_TEST);
+
+      const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+      const fov = Math.PI / 4; 
+      const near = 1;
+      const far = 2000;
+
+      // Set up projection matrix
+      var projectionMatrix = m4.perspective(fov, aspect, near, far);
+
+      // Set up camera matrix, and get the view matrix relative to the camera
+      const cameraMatrix = m4.lookAt(cameraPosition, target, up);
+      const viewMatrix = m4.inverse2(cameraMatrix);
+      
+      // This is for the light to be at fixed location when the camera moves
+      const viewLightDirection = m4.transformDirection(viewMatrix, lightDirection);
+
+      const modelViewMatrix = m4.multiply(viewMatrix, cube.modelMatrix);
+      const normalMatrix = m4.transpose(m4.inverse2(modelViewMatrix));
+      
+      // Combined matrix
+      const matrix = m4.multiply(projectionMatrix, modelViewMatrix);
+      
+      cube.draw(matrix, normalMatrix, viewLightDirection, enableShading);
+      // requestAnimationFrame(drawScene);
+    }
+
+    drawScene();
+  }
+
+  main();
